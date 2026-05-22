e14139d23c1ee0260a3cb138805e9caef593f00e
Mei1910 <148560240+Mei1910@users.noreply.github.com>
2026-05-21 22:02:19 -0400

    Refactor initialState and add isValidMove function
    Refactor initialState function to use a predefined error state. Add isValidMove function to validate moves based on current state and defined moves.

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -1,11 +1,12 @@
 type Coord = (Int, Int) -- (Fila, Columna)
 data Move = U | D | L | R deriving (Show, Eq)
 type State = (Coord, Coord, [Coord]) -- (Robot, CajaObjetivo, CajasDeBloqueo)
+err = ((-1,-1), (-1,-1), [])
 --1 
 initialState :: (Coord) -> (Coord) -> [Coord] -> State
 initialState robot obj bloqs
     | valid robot && valid obj && all valid bloqs && noSolapa = (robot, obj, bloqs)
-    | otherwise = ((-1,-1), (-1,-1), [])
+    | otherwise = err
     where
         valid (f,c)
             | f >= 0 && f <= 5 && c >= 0 && c <= 5 = True
@@ -13,3 +14,41 @@ initialState robot obj bloqs
         noSolapa = robot /= obj && all (/= robot) bloqs && all (/= obj) bloqs
 
 --2
+isValidMove :: State -> Move -> Bool -- (2,2) validU 
+isValidMove (robot, obj, bloqs) mov
+    | mov == U = valid (a - 1 , b) _ _
+    | mov == D = valid (a + 1 , b) _ _
+    | mov == L = valid (a , b - 1) _ _
+    | mov == R = valid (a , b + 1) _ _
+    | otherwise = err
+
+    where
+        valid :: State -> Bool
+        valid sta
+            | initialState (robot)(obj)[bloqs]
+
+{-
+isValidMove :: State -> Move -> Bool
+isValidMove (robot, obj, bloqs) mov
+    | not (within newRobot) = False
+    | newRobot == obj = within behindObj && not (occupied behindObj)
+    | newRobot `elem` bloqs = within behindBox && not (occupied behindBox)
+    | otherwise = True
+    where
+        (r, c) = robot
+        newRobot = case mov of
+            U -> (r-1, c)
+            D -> (r+1, c)
+            L -> (r, c-1)
+            R -> (r, c+1)
+        behindObj = case mov of
+            U -> (r-2, c)
+            D -> (r+2, c)
+            L -> (r, c-2)
+            R -> (r, c+2)
+        behindBox = behindObj
+        within (f, col) = f >= 0 && f <= 5 && col >= 0 && col <= 5
+        occupied coord = coord == obj || elem coord bloqs
+
+
+-}
